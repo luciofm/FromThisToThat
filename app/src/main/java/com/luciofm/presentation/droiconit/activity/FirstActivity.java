14096c589eacda1fc2deb058571728de7522cc3f
Lucio Maciel <luciofm@gmail.com>
2015-08-29 00:42:50 -0300

    Enable all slides

--- app/src/main/java/com/luciofm/presentation/droiconit/activity/FirstActivity.java
+++ app/src/main/java/com/luciofm/presentation/droiconit/activity/FirstActivity.java
@@ -4,12 +4,17 @@ import com.luciofm.presentation.droiconit.R;
 import com.luciofm.presentation.droiconit.fragment.ActivityTransitionFragment;
 import com.luciofm.presentation.droiconit.fragment.AwesomeFragment;
 import com.luciofm.presentation.droiconit.fragment.BaseFragment;
+import com.luciofm.presentation.droiconit.fragment.HelloFragment;
+import com.luciofm.presentation.droiconit.fragment.IntroFragment;
+import com.luciofm.presentation.droiconit.fragment.LayoutTransitionFragment;
 import com.luciofm.presentation.droiconit.fragment.MoreTransitionManagerFragment;
 import com.luciofm.presentation.droiconit.fragment.MoviesFragment;
+import com.luciofm.presentation.droiconit.fragment.MyselfFragment;
 import com.luciofm.presentation.droiconit.fragment.QuesionsFragment;
 import com.luciofm.presentation.droiconit.fragment.SceneFragment;
 import com.luciofm.presentation.droiconit.fragment.SlackFragment;
 import com.luciofm.presentation.droiconit.fragment.TransitionManagerFragment;
+import com.luciofm.presentation.droiconit.fragment.WorkFragment;
 
 import java.util.ArrayList;
 
@@ -18,11 +23,11 @@ public class FirstActivity extends BaseActivity {
     @Override
     ArrayList<Class<? extends BaseFragment>> getFragmentsList() {
         ArrayList<Class<? extends BaseFragment>> fragments = new ArrayList<>();
-        /*fragments.add(HelloFragment.class);
+        fragments.add(HelloFragment.class);
         fragments.add(MyselfFragment.class);
         fragments.add(WorkFragment.class);
         fragments.add(IntroFragment.class);
-        fragments.add(LayoutTransitionFragment.class);*/
+        fragments.add(LayoutTransitionFragment.class);
         fragments.add(TransitionManagerFragment.class);
         fragments.add(MoreTransitionManagerFragment.class);
         fragments.add(SceneFragment.class);
